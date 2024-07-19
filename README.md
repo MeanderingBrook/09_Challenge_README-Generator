ac67de7a54d04fdadec19507ba599471a4f0f4a6
Meandering Brook <jaguar_rebuke_0g@icloud.com>
2024-07-19 13:21:12 -0400

    Draft README, License Badge URL Declaration

--- README.md
+++ README.md
@@ -1,5 +1,133 @@
-# 09_Challenge_README-Generator
+# Full-Stack Developer Bootcamp Module 09 - Challenge: README Generator
 
-Project Title 99999 
-Apache License Version 2.0 
-Miscellaneous 9999 
+## Table of Contents
+
+- [Description](#description)
+- [Install](#install)
+- [Usage](#usage)
+- [Generator](#generator)
+- [Badge](#badge)
+- [Credit and Source Code](#credits-and-code-source)
+- [Related Efforts](#related-efforts)
+- [Maintainers](#maintainers)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [GitHub Repository Location (HTTPS)](#github-repository-location-https)
+- [GitHub Pages Location](#github-pages-location)
+- [Application Screenshots](#application-screenshots) 
+
+
+## Description
+
+The README Generator is a Node.js Command-Line Interface (CLI) based application that enables Users to quickly and professionally create Project README files, in Markdown (.md) format by answering a simple string of questions regarding the User's Project.  
+
+The README Generator enhances the generated Markdown file with programattically-generated colored badges, and URL references.
+
+
+The Dashboard places accurate, timely Weather information at Users' fingertips, utilizing the industry-leading Weater information of OpenWeather through responsive through flexible OpenWeather APIs.
+
+As developed, the Weather Dashboard is only accessible through a local instance, specific to each accessing computer.
+
+Weather Dashboard data is "live," through active refreshes using OpenWeather APIs, but historical location Searches, are held in the Local Storage and, consequently, not currently accessible through the Web.
+
+The Weather Dashboard source code is published on GitHub, and may be cloned to individual User computers to instantiate local versions.
+
+Weather Dashboard historical location Searches are recorded through a City search entry form availabe in the Dashboard itself and aggregated and displayed in the Dashboard sidebar UI.
+
+The app utlizes an HTML-based Form to ingest User search input, JavaScript to push location Searches to Local Storage, and dynamically update the Dashboard with new Search locations, and CSS to style the site. JavaScript is also used to drive OpenWeather API calls, and responsive behavior, such as Search history display updates.
+
+Because the Weather Dashboard is held in Local Storage, location Search entries will persist across User sessions on individual, hosting computers.
+
+
+## Installation
+
+The Weather Dashboard source code may be downloaded from its GitHub repository, and run directly by a hosting computer.
+
+No Installation of the Weather Dashboard is required; the app is fully browser-based, accessed through any modern browser that supports JavaScript.
+
+
+## Usage
+
+The Weather Dashboard is intended for use by one or more Users to view personalized Weather information specific to User-selected locations (Cities) and maintain a quick-access history of location Searches that will be held locally, persisting across multiple sessions. 
+
+All Users of the Weather Dashboard on a local computer will have full visibility to all Dashboard location Searches.
+
+
+## Credits and Code Source
+
+Code, where referenced from a third-party Source, is noted in Comments accompanying the relevant Code lines.
+
+
+## License
+
+Copyright <YEAR> <COPYRIGHT Chris Milazzo>
+
+
+MIT License
+
+Copyright (c) 2024 MeanderingBrook
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+## Badges
+
+N / A
+
+
+## Features
+
+N / A
+
+
+## How to Contribute
+
+N / A
+
+
+## Tests
+
+API parameter testing and data evaluation, see test.js (Archive)
+
+JavaScript-created Dashboard presentation of Weather serach entries as &lt;ul&gt; to confirm searchHistory (Array) update.
+
+`console.log` output of OpenWeather API data specific to called locations and Search history data on Search requests; see script.js.
+
+
+## GitHub Repository Location (HTTPS)
+
+https://github.com/MeanderingBrook/06_Challenge_Weather-Dashboard.git
+
+
+## GitHub Pages Location
+
+https://meanderingbrook.github.io/06_Challenge_Weather-Dashboard/
+
+
+## Application Screenshots
+
+![Weather Dashboard: Empty Dashboard Screenshot](./assets/images/Weather-Dashboard_Empty-Dashboard_Screenshot.png?raw=true "Weather Dashboard: Empty Dashboard")
+
+![Weather Dashboard: Populated Dashboard Screenshot 01 - San Francisco](./assets/images/Weather-Dashboard_Populated-Dashboard_Screenshot-01.png?raw=true "Weather Dashboard: Populated Dashboard  - San Francisco")
+
+![Weather Dashboard: Populated Dashboard Screenshot 02 - Des Moines](./assets/images/Weather-Dashboard_Populated-Dashboard_Screenshot-02.png?raw=true "Weather Dashboard: Populated Dashboard  - Des Moines")
+
+![Weather Dashboard: Populated Dashboard Screenshot 03 - Miami](./assets/images/Weather-Dashboard_Populated-Dashboard_Screenshot-03.png "Weather Dashboard: Populated Dashboard  - Miami")
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,52 +1,180 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(projectLicense) {
-  let licenseBadge = "";
+  let licenseBadge;
 
   switch (projectLicense) {
     case "Apache License Version 2.0":
       console.log("genMD - Line 08: APACHE LICENSE");
 
-      licenseBadge = { name: "Apache+v2", color: "blue" };
+      licenseBadge = { name: "Apache_v2", color: "blue" };
 
-      badgeContent = `https://img.shields.io/badge/License-${licenseBadge.name}-${licenseBadge.color}`;
-      console.log("genMD - Line 11: APACHE BADGE", badgeContent);
+      break;
+    case "GNU General Public License Version 3.0":
+      console.log("genMD - Line 14: GNU v3");
 
-      // return badgeContent;
-      return `(https://img.shields.io/badge/License-${licenseBadge.name}-${licenseBadge.color})`;
-
+      licenseBadge = { name: "GNU_v3", color: "yellow" };
       break;
     case "MIT License":
-      console.log("genMD - Line 11: MIT LICENSE");
+      console.log("genMD - Line 19: MIT LICENSE");
 
-      licenseBadge = { name: "MIT", color: "green" };
-
-      badgeContent = `https://img.shields.io/badge/License-${licenseBadge.name}-${licenseBadge.color}`;
-      console.log("genMD - Line 20: APACHE BADGE", badgeContent);
-
-      // return badgeContent;
-      return `(https://img.shields.io/badge/License-${licenseBadge.name}-${licenseBadge.color})`;
+      licenseBadge = { name: "MIT", color: "darkred" };
+      console.log("genMD - Line 22:", licenseBadge);
 
       break;
+    case "BSD 2-Clause 'Simplified' License":
+      licenseBadge = { name: "BSD-2_Clause", color: "purple" };
+      break;
+    case "BSD 3-Clause 'New' or 'Revised' License":
+      licenseBadge = { name: "BSD-3_Clause", color: "orange" };
+      break;
+    case "Boost Software License":
+      licenseBadge = { name: "Boost_Software", color: "lightblue" };
+      break;
+    case "Creative Commons Zero Version 1.0 Universal":
+      licenseBadge = { name: "CC_Universal", color: "lightgreen" };
+      break;
+    case "Eclipse Public License Version 2.0":
+      licenseBadge = { name: "Eclipse_v2", color: "darkblue" };
+      break;
+    case "GNU Affero General Public License Version 3.0":
+      licenseBadge = { name: "GNU_Affero", color: "darkgreen" };
+      break;
+    case "GNU General Public License Version 2.0":
+      licenseBadge = { name: "GNU_v2", color: "darkorange" };
+      break;
+    case "GNU Lesser General Public License Version 3.0":
+      licenseBadge = { name: "GNU_Lesser", color: "green" };
+      break;
+    case "Mozilla Public License Version 2.0":
+      licenseBadge = { name: "Mozilla_v2", color: "fuschia" };
+      break;
+    case "The Unlicense":
+      licenseBadge = { name: "Unlicense", color: "darkyellow" };
+      break;
     default:
-      console.log("genMD - Line 26: NO LICENSE");
+      licenseBadge = "";
+    // console.log("genMD - Line 57: No License Assigned");
   }
+
+  badgeURL = `https://img.shields.io/badge/License-${licenseBadge.name}-${licenseBadge.color}`;
+  console.log("genMD - Line 60:", badgeURL);
+
+  console.log("genMD - Line 63:");
+  console.log(typeof badgeURL);
+
+  return badgeURL;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(projectLicense) {
+  let licenseURL;
+
+  switch (projectLicense) {
+    case "Apache License Version 2.0":
+      // console.log("genMD - Line 77: APACHE LICENSE");
+
+      licensePath = "Apache-2.0";
+      break;
+    case "GNU General Public License Version 3.0":
+      // console.log("genMD - Line 83: GNU v3");
+
+      licensePath = "gpl-3.0";
+      break;
+    case "MIT License":
+      // console.log("genMD - Line 88: MIT LICENSE");
+
+      licensePath = "MIT";
+      break;
+    case "BSD 2-Clause 'Simplified' License":
+      licensePath = "BSD-2-Clause";
+      break;
+    case "BSD 3-Clause 'New' or 'Revised' License":
+      licensePath = "BSD-3-Clause";
+      break;
+    case "Boost Software License":
+      // Non-Standard URL
+      licensePath = "https://www.boost.org/LICENSE_1_0.txt";
+      break;
+    case "Creative Commons Zero Version 1.0 Universal":
+      // Non-Standard URL
+      licensePath = "http://creativecommons.org/publicdomain/zero/1.0/";
+      break;
+    case "Eclipse Public License Version 2.0":
+      licensePath = "EPL-1.0";
+      break;
+    case "GNU Affero General Public License Version 3.0":
+      licensePath = "agpl-3.0";
+      break;
+    case "GNU General Public License Version 2.0":
+      licensePath = "old-licenses/gpl-2.0.en.html";
+      break;
+    case "GNU Lesser General Public License Version 3.0":
+      licensePath = "lgpl-3.0";
+      break;
+    case "Mozilla Public License Version 2.0":
+      licensePath = "MPL-2.0";
+      break;
+    case "The Unlicense":
+      // Non-Standard URL
+      licensePath = "http://unlicense.org/";
+  }
+
+  if (projectLicense === "Boost Software License") {
+    licenseURL = "https://www.boost.org/LICENSE_1_0.txt";
+    console.log("genMD - Line 130:", licenseURL);
+  } else if (projectLicense === "Creative Commons Zero Version 1.0 Universal") {
+    licenseURL = "http://creativecommons.org/publicdomain/zero/1.0/";
+    console.log("genMD - Line 133:", licenseURL);
+  } else if (projectLicense === "The Unlicense") {
+    licenseURL = "http://unlicense.org/";
+    console.log("genMD - Line 136:", licenseURL);
+  } else {
+    licenseURL = `https://opensource.org/licenses/${licensePath}`;
+    console.log("genMD - Line 139:", licenseURL);
+  }
+
+  return licenseURL;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(projectLicense) {
+  let licenseDecl;
+  const licenseName = projectLicense;
+  console.log("genMD - Line 150:", licenseName);
+
+  const licenseURL = renderLicenseLink(projectLicense);
+  console.log("genMD - Line 153:", licenseURL);
+
+  licenseDecl = `This Application is Licensed under the terms of ${licenseName}. \nPlease refer to the License terms at [License](${licenseURL} site.)`;
+  console.log("genMD - Line 156:", licenseDecl);
+
+  return licenseDecl;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  // return `# ${data.title}
-  renderLicenseBadge(data.projectLicense);
+// function generateMarkdown(data) {
+function generateMarkdown(answers) {
+  let licenseBadge = renderLicenseBadge(answers.projectLicense);
+  console.log("genMD - Line 161:", licenseBadge);
+  // return licenseBadge;
 
-  // `;
+  licenseURL = renderLicenseLink(answers.projectLicense);
+  console.log("genMD - Line 165:", licenseURL);
+  // return licenseURL;
+
+  licenseDecl = renderLicenseSection(answers.projectLicense);
+  console.log("genMD - Line 169:", licenseDecl);
+  // return licenseDecl;
+
+  return `
+# ${answers.projectTitle} \n
+## ${licenseBadge} \n
+## ${licenseURL} \n
+## ${licenseDecl} \n
+  `;
 }
 
 module.exports = generateMarkdown;
